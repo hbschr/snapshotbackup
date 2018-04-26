a257612a1774e15bcf10b586bceb96e3985e1f0b
Heiko Hübscher <heiko.huebscher@gmail.com>
2018-04-26 17:41:24 +0200

    [doc] include private members in documentation

--- doc/conf.py
+++ doc/conf.py
@@ -46,6 +46,9 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
+# include private menbers in autodoc.
+autodoc_default_flags = ['private-members']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
--- doc/index.rst
+++ doc/index.rst
@@ -4,7 +4,7 @@
    contain the root `toctree` directive.
 
 Welcome to snapshotbackup's documentation!
-=======================================
+==========================================
 
 .. toctree::
    :maxdepth: 2
