01eb147ca5b70dd3955c28569b63c92641fafd49
Heiko Hübscher <heiko.huebscher@gmail.com>
2018-04-28 00:16:15 +0200

    [py] intro lockfile to prevent parallel syncing in same directory
    closes #3

---
+++ snapshotbackup/lock.py
@@ -0,0 +1,64 @@
+from os import remove
+from os.path import join as join
+
+
+_lockfilename = '.sync_lock'
+
+
+class LockError(Exception):
+    lockfile: str
+
+    def __init__(self, lockfile):
+        self.lockfile = lockfile
+        self.message = f'cannot lock, `{self.lockfile}` already exists'
+
+    def __str__(self):
+        return f'LockError: {self.message}'
+
+
+class Lock(object):
+    """lockfile as context manager
+
+    :raise LockError: when lockfile already exists
+
+    usage:
+        try:
+            with Lock(path):
+                # do something
+        except LockError:
+            # couldn't do anything, already locked
+    """
+
+    _lockfile: str
+    """full path to the lockfile"""
+
+    def __init__(self, path):
+        """initialize lock
+
+        :param path str: path where lockfile shall be created
+        """
+        self._lockfile = join(path, _lockfilename)
+
+    def __enter__(self):
+        """enter locked context
+
+        :raise LockError: when already locked
+        """
+        if self._is_locked():
+            raise LockError(self._lockfile)
+        open(self._lockfile, 'w').close()
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        """exit locked context, lockfile will be removed"""
+        remove(self._lockfile)
+
+    def _is_locked(self):
+        """checks if lockfile already exists
+
+        :return bool:
+        """
+        try:
+            open(self._lockfile, 'r').close()
+            return True
+        except FileNotFoundError as e:
+            return False
