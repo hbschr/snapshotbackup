ba1a85aff0c9efe3c00c7041d209bbdd77024909
Heiko Hübscher <heiko.huebscher@gmail.com>
2018-05-07 19:08:57 +0200

    [refac] mv `Lock` to `BackupDir`
    `_sync_dir` and `_sync_lockfile` defined at same place.

--- snapshotbackup/backupdir.py
+++ snapshotbackup/backupdir.py
@@ -1,13 +1,13 @@
 import os
-from os.path import abspath, isdir, join
+from os.path import abspath, dirname, isdir, join
 
-from snapshotbackup.backup import Backup
-from .exceptions import BackupDirError
-from .lock import Lock
+from .backup import Backup
+from .exceptions import BackupDirError, LockedError, LockPathError
 from .shell import create_subvolume, is_btrfs
 from .timestamps import get_timestamp, is_timestamp
 
 _sync_dir = '.sync'
+_sync_lockfile = '.sync_lock'
 
 
 class BackupDir(object):
@@ -97,3 +97,61 @@ class BackupDir(object):
             else:
                 backups.insert(0, Backup(dir, self.dir, retain_all_after, retain_daily_after, next=backups[0]))
         return backups
+
+
+class Lock(object):
+    """lockfile as context manager
+
+    :raise LockedError: when lockfile already exists
+    :raise LockPathError: when lockfile cannot be created (missing dir)
+
+    >>> import tempfile
+    >>> from os.path import join
+    >>> from snapshotbackup.backupdir import Lock
+    >>> with tempfile.TemporaryDirectory() as path:
+    ...     with Lock(path):
+    ...         pass
+    >>> with tempfile.TemporaryDirectory() as path:
+    ...     with Lock(path):
+    ...         with Lock(path):
+    ...             pass
+    Traceback (most recent call last):
+    snapshotbackup.exceptions.LockedError: ...
+    >>> with tempfile.TemporaryDirectory() as path:
+    ...     with Lock(join(path, 'nope')):
+    ...         pass
+    Traceback (most recent call last):
+    snapshotbackup.exceptions.LockPathError: ...
+    """
+
+    _lockfile: str
+    """full path to the lockfile"""
+
+    def __init__(self, path):
+        """initialize lock
+
+        :param str path: path where lockfile shall be created
+        """
+        self._lockfile = join(path, _sync_lockfile)
+
+    def __enter__(self):
+        """enter locked context
+
+        :raise LockedError: when already locked
+        :raise LockPathError: when path of lockfile is not found
+        :raise OSError: others may occur
+        """
+        try:
+            open(self._lockfile, 'r').close()
+            raise LockedError(self._lockfile)
+        except FileNotFoundError as e:
+            pass
+
+        try:
+            open(self._lockfile, 'w').close()
+        except FileNotFoundError as e:
+            raise LockPathError(dirname(self._lockfile))
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        """exit locked context, lockfile will be removed"""
+        os.remove(self._lockfile)
