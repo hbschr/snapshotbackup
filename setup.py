c66e8f1f12ad9ee7c32d89e9047779d309e32568
Heiko Hübscher <heiko.huebscher@gmail.com>
2018-05-30 02:27:56 +0200

    [deps] rm unneeded dependency `setuptools-scm`
    ref d3099afc13dd5294eb22becb3687c903228f4e16

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
         'dateparser>=0.7.0',
         'python-dateutil>=2.7.2',
         'pytz>=2018.4',  # needed by `dateparser`
-        'setuptools-scm>=2.0.0',
     ],
     extras_require={
         'journald': ['systemd-python>=234'],
