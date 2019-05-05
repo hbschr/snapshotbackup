7a814f1227bdddd96f10cb196fa2a0a6ffa868d2
Heiko Hübscher <heiko.huebscher@gmail.com>
2019-05-05 22:09:40 +0200

    [package] add python versions 3.6 and 3.8
    ref 0f97f52f447ad0c79f34d1b5da9466cb5a57e0c1
    ref dedbc71cb242f8e4b88ec2630dcba891f361f70d

--- setup.py
+++ setup.py
@@ -58,6 +58,8 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: System :: Archiving :: Backup',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
