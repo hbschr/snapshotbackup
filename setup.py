21de847ba55d0b6a07a7dbb8a51ab6751d9a7dde
Heiko Hübscher <heiko.huebscher@gmail.com>
2019-05-07 22:20:49 +0200

    [deps] rm pytz, dateparser has its own dependency
    reverts 6abd4a772396b00d481b43f6035f883a74a5b807

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
         'humanfriendly>=4.17',
         'psutil>=5.4.8',
         'python-dateutil>=2.7.2',
-        'pytz>=2018.4',  # needed by `dateparser`
         'xdg>=4.0.0',
     ],
     extras_require={
