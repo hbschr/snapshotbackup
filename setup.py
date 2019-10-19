88bdb4d13ef6e1e5ce4da202332c5ed11f9fe1ce
Heiko Hübscher <heiko.huebscher@gmail.com>
2019-10-19 21:28:34 +0200

    [package] remove support for python 3.6
    ref 7a814f1227bdddd96f10cb196fa2a0a6ffa868d2
    ref 0f97f52f447ad0c79f34d1b5da9466cb5a57e0c1

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: System :: Archiving :: Backup',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
